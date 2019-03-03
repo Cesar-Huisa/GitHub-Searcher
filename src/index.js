5c3a8261163024c1e112f5c861cbb5576ff30f03
Cesar-x1 <darkheart.x1@outlook.com>
2019-03-02 20:12:24 -0500

    GraphQL: servidor no responde

--- src/index.js
+++ src/index.js
@@ -9,18 +9,22 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from "react-apollo";
 require('dotenv').load();
 
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 const httpLink = new HttpLink({
-    uri:'https://api.github.com/graphql',
+    uri: GITHUB_BASE_URL,
     headers: {
-      Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      authorization: `Bearer ${
+        process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+        }`
     },
-  });
-  const cache = new InMemoryCache();
-  
-  const client = new ApolloClient({
-    link: httpLink,
-    cache,
-  });
+    });
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+link: httpLink,
+cache,
+});
 
 
 var a = document.getElementById('tfnewsearch');
